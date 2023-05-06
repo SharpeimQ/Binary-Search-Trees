402c9d14a3bfffd381ec3b7ed5f7e0c6fd8968f0
SharpeimQ <SharpHaiyan@gmail.com>
2023-05-06 15:54:35 -0400

    Add driver function to test functionality

--- index.rb
+++ index.rb
@@ -9,16 +9,22 @@ while array.length < 15
 end
 
 bst = Tree.new(array)
+
 bst.pretty_print
 p bst.balanced?
 p bst.pre_order
 p bst.post_order
 p bst.inorder
 
-bst.pretty_print
-
 bst.insert(120)
 bst.insert(130)
 bst.insert(150)
 
 bst.pretty_print
+p bst.balanced?
+
+bst.rebalance
+p bst.balanced?
+bst.pre_order
+bst.post_order
+bst.inorder
